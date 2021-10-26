297b53e1704f655094fd04677f81e0d1cec79352
Daniel JANG <daniel_media@daum.net>
2021-10-26 21:15:59 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by JangDaniel on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
